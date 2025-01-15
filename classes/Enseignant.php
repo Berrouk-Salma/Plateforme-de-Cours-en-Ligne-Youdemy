3fb9be59fa7c6f667f1b88b641d6d18595c7e32c
Berrouk-Salma <salmaberrouk4@gmail>
2025-01-15 15:04:55 +0100

    edit class ensiegnant

--- classes/Enseignant.php
+++ classes/Enseignant.php
@@ -1,28 +1,11 @@
 <?php
-namespace Youdemy;
 
-class Enseignant extends Utilisateur
-{
-    public function ajouterCours($titre, $description, $contenu, $categorieId)
-    {
+    require_once __DIR__ .'./Utilisateur.php';
+
+    class Enseignant extends Utilisateur {
+
         
-        $cours = new Cours($titre, $description, $contenu, $categorieId, $this->getId());
-        return $cours->save();
+
     }
 
-    public function modifierCours($coursId, $titre, $description, $contenu, $categorieId)
-    {
-   
-        $cours = new Cours($titre, $description, $contenu, $categorieId, $this->getId());
-        $cours->setId($coursId);
-        return $cours->update();
-    }
-
-    public function supprimerCours($coursId)
-    {
-    
-        $cours = new Cours();
-        $cours->setId($coursId);
-        return $cours->delete();
-    }
-}
+?>
